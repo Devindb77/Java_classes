d8de23f262936351ab7e6e95debb0b3140638b8c
Devindb77 <39813726+Devindb77@users.noreply.github.com>
2018-06-08 15:21:39 -0500

    Add files via upload

--- hw9.js
+++ hw9.js
@@ -1,31 +1,61 @@
 class Dog {
-    constructor(food,drink) {
+    constructor(food, drink) {
         this._drink = drink;
         this._food = food;
     }
-    get Food() {
-       return this._food
+    get food() {
+        console.log(`we are getting ${this._food}`);
+        return this._food;
     }
     get drink() {
-        return this._drink
+        console.log(`im dehydrated ${this._drink}`);
+        return this._drink;
     }
-    eat(Steak) {
-        this._food
-    console.log(`${this._food} The dog eats`);
+    eat() {
+        console.log(` The dog eats ${this._food}`);
     }
-    drink() {
-        console.log()
-    }
+
 
 }
-var drink = new Dog("ribs", "Water");
-console.log(drink.drink);
-console.log(drink.food);
 
-var food = new Dog("ribs", "juice");
+var food = new Dog("steak", "juice");
 console.log(food.food);
-drink.eat(Steak);
-drink.drink();
+food.eat();
+food.drink;
 
-food.eat(Steak);
-food.drink();
+
+
+class cat extends Dog {
+    constructor(play, sleep, food, drink) {
+        super(food, drink)
+        this._play = play;
+        this._sleep = sleep;
+        this._food = food;
+        this._drink = drink;
+    }
+    get play() {
+        console.log(`Time to play ${this._play}`);
+        return this._play;
+    }
+    get sleep() {
+        console.log(`it is ${this._sleep}`);
+        return this._sleep;
+    }
+    get food() {
+        return this._food;
+    }
+    get drink() {
+        return this._drink;
+    }
+    get play() {
+        return this._play
+    }
+    sleep() {
+        console.log(`the dog is ${this._sleep}`);
+    }
+}
+var Henry = new Dog("chicken", "Sleepytime", "ball", "water");
+console.log(Henry.play);
+console.log(Henry.sleep);
+console.log(Henry.food);
+console.log(Henry.drink);
