e5624a77038ad36ee76c73854ffb048c5659ffd1
Devindb77 <39813726+Devindb77@users.noreply.github.com>
2018-06-12 09:28:25 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace MyStart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ready or not");
+            Console.WriteLine("Gonna find you");
+            Console.ReadKey();
+
+            string reduce = "Im at home where you at";
+            string didi = "IDK COME FIND ME!!";
+            string DODO = "Chill down didi down down";
+            Console.WriteLine(reduce);
+            Console.WriteLine(didi);
+            Console.WriteLine(DODO);
+            Console.ReadKey();
+
+            int deadit = 2156;
+            int cutit = 4576;
+            Console.Write(deadit);
+            Console.Write(cutit);
+            Console.ReadKey();
+
+            float ease = 7.9f;
+            double redred = 4.3E+7;
+            bool tedted = true;
+            Console.ReadKey();
+
+            Console.WriteLine("my number is {0} and {2} i need to chill {1}", deadit,DODO, reduce);
+            Console.ReadKey();
+
+            Console.Write("baby tell me you down i think you down are you down");
+            string das = Console.ReadLine();
+            Console.WriteLine(das);
+            Console.ReadKey();
+
+            Console.WriteLine(6 < 9);
+            Console.WriteLine("Madder" == "Madder");
+            Console.WriteLine(8 < 6);
+            Console.WriteLine(!true);
+            Console.ReadKey();
+            
+        }
+    }
+}
