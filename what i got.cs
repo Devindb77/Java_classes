c6cffd5fb3911a47bc680f4680a1e7268545dc68
Devindb77 <39813726+Devindb77@users.noreply.github.com>
2018-06-23 22:09:51 -0500

    Add files via upload

---
+++ what i got.cs
@@ -0,0 +1,95 @@
+﻿using nameless;
+using System;
+using System.Diagnostics;
+
+namespace turnup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessStartInfo DONCANNON = new ProcessStartInfo("C:/Program Files (x86)/Google/Chrome/Application/Chrome.exe");
+            DONCANNON.Arguments = "https://www.google.com/search?biw=1366&bih=662&tbm=isch&sa=1&ei=N-AuW9bkH-b-jwTp84n4Bg&q=herocannon&oq=herocannon&gs_l=img.3..0i7i30k1l2j0i8i7i30k1.11575.14500.0.14871.4.4.0.0.0.0.182.506.1j3.4.0....0...1c.1.64.img..1.3.428....0.6aKbvJICLZY#imgrc=jOTXgM4vsqUm7M:";
+
+            ProcessStartInfo death0slayer = new ProcessStartInfo("C:/ Program Files(x86) / Google / Chrome / Application / Chrome.exe");
+            death0slayer.Arguments = "https://www.google.com/search?biw=1366&bih=613&tbm=isch&sa=1&ei=HtwuW8GJH8iSjwSos4GYBg&q=death0slayer&oq=death0slayer&gs_l=img.3...530377.537621.0.537728.22.15.1.1.1.0.140.1139.7j6.13.0....0...1c.1.64.img..9.7.726.0..0j0i67k1j0i5i30k1j0i8i30k1j0i24k1.0.m3t93D150RI#imgrc=BhnxMN4b917fbM";
+
+            ProcessStartInfo Lightgeneral = new ProcessStartInfo("C:/ Program Files(x86) / Google / Chrome / Application / Chrome.exe");
+            Lightgeneral.Arguments = "https://en.wikipedia.org/wiki/Raiden_(Mortal_Kombat)#/media/File:RaidenMKXrender.png";
+
+            ProcessStartInfo Lighting = new ProcessStartInfo("C:/ Program Files(x86) / Google / Chrome / Application / Chrome.exe");
+            Lighting.Arguments = "https://www.google.com/search?biw=1366&bih=662&tbm=isch&sa=1&ei=N-AuW9bkH-b-jwTp84n4Bg&q=herocannon&oq=herocannon&gs_l=img.3..0i7i30k1l2j0i8i7i30k1.11575.14500.0.14871.4.4.0.0.0.0.182.506.1j3.4.0....0...1c.1.64.img..1.3.428....0.6aKbvJICLZY#imgrc=jOTXgM4vsqUm7M:";
+
+            ProcessStartInfo Deathblesser = new ProcessStartInfo("C:/ Program Files(x86) / Google / Chrome / Application / Chrome.exe");
+            Deathblesser.Arguments = "https://en.wikipedia.org/wiki/Judge_Death#/media/File:JudgeDeathIrving.jpg";
+
+            ProcessStartInfo ruinraiserattack = new ProcessStartInfo("C:/ Program Files(x86) / Google / Chrome / Application / Chrome.exe");
+            ruinraiserattack.Arguments = "https://www.google.com/search?biw=1366&bih=662&tbm=isch&sa=1&ei=TOMuW-joGsz4jwTH143gBg&q=earth+attack+avatar&oq=earth+attack+avatar&gs_l=img.3...87756.100895.0.101760.20.19.0.1.1.0.216.1885.5j12j1.18.0....0...1c.1.64.img..1.13.1560.0..0j0i67k1j0i10i67k1j0i30k1j0i5i30k1j0i8i30k1.0.YjasHBK4TnQ#imgrc=aNCfN_tAe5Z2MM:";
+
+            ProcessStartInfo King = new ProcessStartInfo("C:/ Program Files(x86) / Google / Chrome / Application / Chrome.exe");
+            King.Arguments = "https://www.google.com/search?biw=1366&bih=662&tbm=isch&sa=1&ei=tOMuW9TFC-ymjwSzmKXwBA&q=king&oq=king&gs_l=img.3..0i67k1l8j0j0i67k1.154517.155526.0.156177.5.4.0.1.1.0.135.362.0j3.3.0....0...1c.1.64.img..1.4.420.0...0.VdsYNBy6pnc#imgrc=ItIlGroOhXQ5sM:";
+
+            Process.Start(death0slayer);
+
+            Console.WriteLine("Ahhhh....Finally 1000 year of eternal cast away the King of of all realms has awaken");
+            Console.ReadKey();
+
+            Console.WriteLine("My shadows will decept all before who oppose my power");
+            Console.ReadKey();
+
+            Console.WriteLine("Ye shall surely die");
+            Console.ReadKey();
+
+            Console.Write("Cast dark sprits");
+            Console.ReadKey();
+
+            SuperVillian Newbie = new SuperVillian("DONCANNON", "dark realm", "deathSlayer",100);
+
+            Process.Start(DONCANNON);
+
+            Console.Write("shawdow makers fetch Weapon");
+            Console.ReadKey();
+
+            Console.WriteLine("look here we find the masters doom machine");
+            Console.ReadKey();
+
+            Console.WriteLine("Yes,Yes he will be very Pleased");
+            Console.ReadKey();
+
+            Console.Write("returns to masters den");
+            Console.ReadLine();
+
+            Console.WriteLine("My lord we Have returned with your masterpiece");
+            Console.ReadKey();
+
+            Console.WriteLine("is that so show me what do you have");
+            Console.ReadKey();
+
+            Console.WriteLine("Behold The [enter weapon type]");
+            string _villianweapon = Console.ReadLine();
+            Console.ReadKey();
+
+            Console.WriteLine("i shall become one with my ultimate weapon and i will rule all");
+            Console.ReadKey();
+
+            Console.Write("inserts weapon in arm");
+            Console.ReadLine();
+
+
+            Console.WriteLine("yes the power surges inside of me");
+            int Gainstrength = Newbie._Strength + 99;
+            Console.WriteLine(Gainstrength);
+            Console.ReadKey();
+
+            Console.WriteLine("ahhhh what a powerful bliss it is time to make my move");
+            Console.ReadKey();
+
+            Console.Write("Blasts out of den");
+            Console.ReadLine();
+
+            Console.Write("arrives in space staring at globe");
+            Console.ReadLine();
+         
+        }
+    }
+    }
